face80f37921d05f1bedd3198779cefb1fd652bd
Pierdomenico Apruzzese <pieroapruzzese@gmail.com>
2021-04-28 13:52:03 +0200

    styled a bit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 //import Side from "./Side";
 import Pier from "../assets/pier1.jpeg";
+import Post from "../assets/pierman.png";
 import { getPier } from "../api/api";
 
 export default function Home() {
@@ -19,34 +20,97 @@ export default function Home() {
     }
   };
   return (
-    <Container>
+    <Container className="mt-5">
       <Row>
-        <Col sm={2}>
-          <div className="position-relative">
-            <img
-              src={Pier}
-              style={{ width: "75%" }}
-              alt="Pier"
-              className="position-absolute"
-            />
-          </div>
+        <Col sm={{ offset: 1, span: 4 }}>
+          <img
+            src={Pier}
+            className="animate__animated animate__zoomIn"
+            style={{ width: "75%" }}
+            alt="Pier"
+          />
         </Col>
-        <Col sm={8}>
-          <div style={{ width: "500px" }}>
-            <Form.Group>
+        <Col sm={3} className="d-flex flex-row">
+          <Form.Group>
+            <div className="d-flex mt-3 mb-5 animate__animated animate__slideInLeft">
               <Form.Control
                 placeholder="/about/Pierdomenico"
                 disabled
                 style={{ cursor: "pointer" }}
                 //onClick={()=>setValue("")}
               />
-              <Button variant="success" onClick={() => fetchPier()}>
+              <Button
+                className="ml-2"
+                variant="success"
+                onClick={() => fetchPier()}
+              >
                 GET
-              </Button>{" "}
-            </Form.Group>
-          </div>
-          {pier && <p>{JSON.stringify({ pier })}</p>}
+              </Button>
+            </div>
+            <div className="d-flex mb-5 animate__animated animate__slideInLeft">
+              <Form.Control
+                placeholder="/projects/Pierdomenico"
+                disabled
+                style={{ cursor: "pointer" }}
+                //onClick={()=>setValue("")}
+              />
+              <Button className="ml-2" disabled variant="success">
+                GET
+              </Button>
+            </div>
+            <div className="d-flex animate__animated animate__slideInLeft">
+              <Form.Control
+                placeholder="/contact/Pierdomenico"
+                disabled
+                style={{ cursor: "pointer" }}
+                //onClick={()=>setValue("")}
+              />
+              <Button className="ml-2" disabled variant="danger">
+                POST
+              </Button>
+            </div>
+          </Form.Group>
         </Col>
+        <Col sm={2}>
+          <img src={Post} style={{ width: "200px" }} alt="post" />
+          <p style={{ fontSize: "10px" }}>Powered by PostPier</p>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        {pier && (
+          <Col
+            className="d-flex-column justify-content-start"
+            sm={{ offset: 2, span: 8 }}
+            style={{ border: "1px solid black", textAlign: "left" }}
+          >
+            {"{"}
+            <p>
+              <strong>name</strong>: {pier.name}{" "}
+            </p>
+            <p>
+              <strong>surname</strong>: {pier.surname}
+            </p>
+            <p>
+              <strong>location</strong>: {pier.location}
+            </p>
+            <p>
+              <strong>about</strong>: {pier.about}
+            </p>
+            <p>
+              <strong>experience</strong>:
+            </p>
+            {pier.experience.map((exp) => (
+              <>
+                <p>
+                  {"   "}
+                  {exp.company} {exp.position} {exp.year}
+                </p>
+                <p>{exp.description}</p>
+              </>
+            ))}
+            {"}"}
+          </Col>
+        )}
       </Row>
     </Container>
   );
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
-import Mondo from "../assets/mondo.png";
+import Post from "../assets/mondo.png";
 import TextAnimation from "react-animate-text";
 
 export default function Navigation() {
   return (
-    <Navbar bg="dark">
-      <Navbar.Brand href="#home">
+    <Navbar bg="light" style={{ height: "10vh" }}>
+      <Navbar.Brand href="#home" className="d-flex align-items-center">
         <img
-          src={Mondo}
-          width="100"
-          height="100"
-          className="d-inline-block align-top"
+          src={Post}
+          style={{
+            height: "150px",
+            position: "relative",
+            top: "2vh",
+            left: "2vh",
+          }}
+          /* className="d-inline-block align-top" */
           alt="logo"
         />
       </Navbar.Brand>
-      <TextAnimation style={{ color: "white" }}>
+      {/* <TextAnimation style={{ color: "white" }}>
         Pierdomenico Apruzzese, web dev
-      </TextAnimation>
-      ;
+      </TextAnimation> */}
     </Navbar>
   );
 }
